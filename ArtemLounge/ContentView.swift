da1a49eca9f0f3f6c11776333f23631cbbe2e11a
Artemaj9 <104516847+Artemaj9@users.noreply.github.com>
2023-04-20 15:11:43 +0300

    add Whats up

--- ArtemLounge/ContentView.swift
+++ ArtemLounge/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, wonderwally!")
+            Text("Hello, wonderwally WHats up?!")
         }
         .padding()
     }
