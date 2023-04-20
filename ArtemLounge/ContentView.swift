9c0c74b7e77b92ae1b3f6785edbb2325e5e7e68d
Artem <moiseev@physics.msu.ru>
2023-04-20 13:08:44 +0300

    Wonderfully!

--- ArtemLounge/ContentView.swift
+++ ArtemLounge/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world Baby!")
+            Text("Hello, wonderwally!")
         }
         .padding()
     }
