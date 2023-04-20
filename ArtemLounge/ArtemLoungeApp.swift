8e9cf7c5f9c65b6a7b836bc6930eaca88b4a6674
Artem <moiseev@physics.msu.ru>
2023-04-20 11:59:50 +0300

    Initial Commit

---
+++ ArtemLounge/ArtemLoungeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtemLoungeApp.swift
+//  ArtemLounge
+//
+//  Created by Artem on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtemLoungeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
